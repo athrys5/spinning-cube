736dc5d63180c05bd1c55e13a2aaa83f80031cbe
athrys <capriotti.andrea00@gmail.com>
2026-01-13 19:11:33 +0100

    first render

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <math.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-#define HEIGHT 40;
-#define WIDTH 80;
-#define DISTANCE 5;
+#define HEIGHT 40
+#define WIDTH 80
+#define DISTANCE 5.0
+#define SCALE 10.0
 
 char screen[HEIGHT][WIDTH];
 
@@ -18,7 +21,10 @@ int matrix[8][3] = {
     {-1, 1, 1}    // 7: front top left
 };
 
-//todo: struct for a point
+void clear(){
+	printf("\033[2J\033[H");
+	fflush(stdout);
+}
 
 void rotateY(float x, float z, float angle_deg, float *new_x, float *new_z) {
     float angle_rad = angle_deg * (M_PI / 180); 
@@ -29,28 +35,45 @@ void rotateY(float x, float z, float angle_deg, float *new_x, float *new_z) {
     *new_z = -x * sin_a + z * cos_a;
 }
 
-void render2D(float x, float y, float z) {
+void get_screen_point(float x, float y, float z, int *col, int *row) {
 	float x_screen = (x * DISTANCE) / (z + DISTANCE);
         float y_screen = (y * DISTANCE) / (z + DISTANCE);	
+	*col = (int)(x_screen * SCALE + WIDTH/2);
+        *row = (int)(-y_screen * SCALE + HEIGHT/2); 		
 }
 
 void clear_screen(){
-	for(int i = 0; i < WIDTH; i++)
-		for(int j = 0; j < HEIGHT; j++)
+	for(int i = 0; i < HEIGHT; i++)
+		for(int j = 0; j < WIDTH; j++)
 			screen[i][j] = ' ';
 }
 
 int main(){
 	float angle = 0;
 	while(1) {
-		angle += 20;
+		clear_screen();
 		for(int i = 0; i < 8; i++) {
-			float x = matrix[i][1];
-			float z = matrix[i][3];
+			float x = matrix[i][0];
+			float y = matrix[i][1];
+			float z = matrix[i][2];
 			float new_x, new_z;
+			int col = 0;
+			int row = 0;
 			rotateY(x, z, angle, &new_x, &new_z);
+			get_screen_point(new_x, y, new_z, &col, &row);
+			if(row >= 0 && row < HEIGHT && col >= 0 && col < WIDTH) screen[row][col] = '*';
 		}
-		break;
+		
+		system("clear");	
+
+		for(int i = 0; i < HEIGHT; i++){
+			for(int j = 0; j < WIDTH; j++){
+				printf("%c", screen[i][j]);
+			}
+			printf("\n");	
+		}
+		usleep(100000);
+		angle += 10;
 	}
 	return 0;
 }
